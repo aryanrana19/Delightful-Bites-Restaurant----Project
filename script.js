06cbd5c908391aef3d394de2ba53e201775956fb
Aryan Rana <itsaryanrana@gmail.com>
2024-04-06 00:14:33 +0530

    reservations

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-const contact = document.getElementById("contact-form")
+// Function to open the modal
+function openModal() {
+    var modal = document.querySelector('.contact-modal');
+    modal.style.display = 'flex';
+}
+
+function closeModal() {
+    var modal = document.querySelector('.contact-modal');
+    modal.style.display = 'none';
+}
+
+
+// Select Seat
+function toggleChair(chair) {
+    chair.classList.toggle('selected');
+}
